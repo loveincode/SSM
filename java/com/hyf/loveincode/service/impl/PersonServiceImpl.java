ddc2590d1401a7313a337e947b03ce1240c763b3
loveincode <huonefl@163.com>
2017-09-18 22:24:06 +0800

    up encache

--- java/com/hyf/loveincode/service/impl/PersonServiceImpl.java
+++ java/com/hyf/loveincode/service/impl/PersonServiceImpl.java
@@ -24,59 +24,50 @@ public class PersonServiceImpl implements IPersonService {
 	@Autowired
 	private PersonMapper personMapper;
 
-	@Override
 	public List<Person> queryAll() {
 		return personMapper.queryAll();
 	}
 
-	@Override
 	public void add(Person person) {
 		personMapper.insert(person);
 	}
 
-	@Override
 	public void delete(Integer id) {
 		personMapper.delete(id);
 	}
 
-	@Override
 	public void update(Person person) {
 		personMapper.update(person);
 	}
 
-	@Override
 	public Person findByName(String name) {
 		return personMapper.findByName(name);
 	}
 
-	@Override
 	public Person findById(Integer id) {
 		return personMapper.findById(id);
 	}
 
-	@Override
 	public Page<Person> listByPage(int pageNo, int pageNum) {
-		Page<Person> page = new Page<Person>(pageNo,pageNum);
+		Page<Person> page = new Page<Person>(pageNo, pageNum);
 
-        int offset = page.getOffsets();
-        RowBounds rowBound = new RowBounds(offset, pageNum);
-        System.err.println(rowBound.getOffset()+" rowbound "+rowBound.getLimit());
-        
-        List<Person> users = personMapper.listByPage(rowBound);
-        page.setRows(users);
-        int total = personMapper.countAll();
-        page.setTotal(total) ;
-        if(offset >= page.getTotal()){
-            page.setPageNo(page.getTotalPages());
-        }
-        return page ;
+		int offset = page.getOffsets();
+		RowBounds rowBound = new RowBounds(offset, pageNum);
+		System.err.println(rowBound.getOffset() + " rowbound " + rowBound.getLimit());
+
+		List<Person> users = personMapper.listByPage(rowBound);
+		page.setRows(users);
+		int total = personMapper.countAll();
+		page.setTotal(total);
+		if (offset >= page.getTotal()) {
+			page.setPageNo(page.getTotalPages());
+		}
+		return page;
 	}
 
-	@Override
 	public List<Person> listByParams(Map<String, Object> params) {
 		List<Person> users = personMapper.listByParams(params);
-		return  users;
+		return users;
 	}
 
-	
 }
