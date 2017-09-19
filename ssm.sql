2a58d304d34c7fcc9a08f234fbfa11c3cbb543f5
loveincode <huonefl@163.com>
2017-09-19 12:09:13 +0800

    up ssm.sql

--- ssm.sql
+++ ssm.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50617
 File Encoding         : 65001
 
-Date: 2017-09-12 16:09:30
+Date: 2017-09-19 12:08:43
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,11 +24,77 @@ CREATE TABLE `person` (
   `name` varchar(255) NOT NULL,
   `age` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of person
 -- ----------------------------
-INSERT INTO `person` VALUES ('1', 'loveincode', '18');
-INSERT INTO `person` VALUES ('2', 'loveincode', '20');
-INSERT INTO `person` VALUES ('3', 'loveincode', '19');
+INSERT INTO `person` VALUES ('6', '中文123', '18');
+INSERT INTO `person` VALUES ('7', '中文123', '23');
+INSERT INTO `person` VALUES ('8', '中文123', '0');
+INSERT INTO `person` VALUES ('10', '123', '0');
+INSERT INTO `person` VALUES ('11', '中文123', '0');
+INSERT INTO `person` VALUES ('12', '222', '0');
+INSERT INTO `person` VALUES ('13', '中文123', '0');
+INSERT INTO `person` VALUES ('14', '中文123', '0');
+INSERT INTO `person` VALUES ('15', '中文', '0');
+INSERT INTO `person` VALUES ('16', '中文123', '0');
+INSERT INTO `person` VALUES ('17', '新都', '0');
+INSERT INTO `person` VALUES ('18', '中文123', '0');
+INSERT INTO `person` VALUES ('19', '123搜索', '0');
+INSERT INTO `person` VALUES ('21', '222', '0');
+
+-- ----------------------------
+-- Table structure for `t_permission`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_permission`;
+CREATE TABLE `t_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `permissionName` varchar(50) DEFAULT NULL,
+  `roleId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `roleId` (`roleId`) USING BTREE,
+  CONSTRAINT `t_permission_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `t_role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_permission
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `t_role`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_role`;
+CREATE TABLE `t_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `roleName` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_role
+-- ----------------------------
+INSERT INTO `t_role` VALUES ('1', 'admin');
+INSERT INTO `t_role` VALUES ('2', 'teacher');
+
+-- ----------------------------
+-- Table structure for `t_user`
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userName` varchar(20) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `roleId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `roleId` (`roleId`) USING BTREE,
+  CONSTRAINT `t_user_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `t_role` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
+INSERT INTO `t_user` VALUES ('1', 'loveincode', '123456', '1');
+INSERT INTO `t_user` VALUES ('2', 'aaa', '12345', '2');
+INSERT INTO `t_user` VALUES ('3', 'bbb', '12345', null);
+INSERT INTO `t_user` VALUES ('4', 'ccc', '12345', null);
